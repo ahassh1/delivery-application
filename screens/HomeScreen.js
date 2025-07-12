cd0bb5a3726957273e266cd10f3cf02241e88729
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-07-12 13:30:27 +0600

    reacticons added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,34 +1,41 @@
 import { View, Text, Image, SafeAreaView } from 'react-native';
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { UserIcon, ChevronDownIcon } from 'react-native-heroicons/outline';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      // headerTitle: 'TESTING',
       headerShown: false,
     });
   }, [navigation]);
 
   return (
-    <SafeAreaView>
-      <Text>
-        {/* header  */}
-        <View className="item-center mx-3 flex-row space-x-2 pb-3">
-          <Image
-            source={{
-              uri: 'https://links.papareact.com/wru',
-            }}
-            className="h-8 w-8 rounded-full bg-gray-300 p-4"
-          />
-          <View>
-            <Text className="text-xs font-bold text-gray-500">Delivery Now!</Text>
-            <Text className="text-xl font-bold">Current Location</Text>
+    <SafeAreaView className="bg-white pt-4">
+      {/* Header */}
+      <View className="flex-row items-center space-x-3 px-4 pb-3">
+        {/* User Avatar */}
+        <Image
+          source={{
+            uri: 'https://links.papareact.com/wru',
+          }}
+          className="h-8 w-8 rounded-full bg-gray-300"
+        />
+
+        {/* Location Info */}
+        <View className="flex-1">
+          <Text className="text-xs font-bold text-gray-500">Delivery Now!</Text>
+          <View className="flex-row items-center">
+            <Text className="mr-1 text-xl font-bold">Current Location</Text>
+            <ChevronDownIcon size={20} color="#00CCBB" />
           </View>
         </View>
-      </Text>
+
+        {/* User Icon */}
+        <UserIcon size={30} color="#00CCBB" />
+      </View>
     </SafeAreaView>
   );
 };
